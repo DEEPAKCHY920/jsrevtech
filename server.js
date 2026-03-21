54e7c1562b125e2b6cdab5adedd07ac3b3c5ee5f
DEEPAK CHOUDARY <technicaldeepak15@gmail.com>
2026-03-21 20:46:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ const cooldownStore = new Map();
 
 // ================= MAIL CONFIG =================
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
